ef7bf2f1db6875beef5e072f5d09c2380e70fe28
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-05-15 19:57:19 -0400

    mas cambios

--- server.js
+++ server.js
@@ -11,26 +11,6 @@ import authorRoutes from './routes/authorRoutes.js';
 import historyRoutes from './routes/historyRoutes.js';
 
 const corsOptions = {
-<<<<<<< HEAD
-  origin: function (origin, callback) {
-    // Permitir localhost y todas las versiones de Vercel
-    const allowedOrigins = [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:5174',
-      'http://localhost:5173',
-      'https://historias-api-crud.vercel.app',
-      'https://historias-eight.vercel.app',
-      'https://eco-museo-api.vercel.app'
-    ];
-
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-=======
   origin: [
     'http://localhost:3000',
     'http://localhost:3001',
@@ -39,15 +19,7 @@ const corsOptions = {
     'https://eco-museo-api.vercel.app',
     'https://historias-api-crud.vercel.app',
     '*'
-  ],
->>>>>>> parent of 3309c1d (mini cambios)
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
-  exposedHeaders: ['Content-Length'],
-  maxAge: 3600,
-  preflightContinue: false,
-  optionsSuccessStatus: 204
+  ]
 };
 
 const app = express();
