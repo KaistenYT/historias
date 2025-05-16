a51a33b90fffba525cf6f9571db4084c8824cf93
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-05-15 20:03:34 -0400

    rutas

--- routes/actorRoutes.js
+++ routes/actorRoutes.js
@@ -4,11 +4,11 @@ import { ActorController } from '../controller/actorController.js';
 const router = express.Router();
 
 // Define las rutas para los recursos de actor
-router.get('/list', ActorController.getAllActors);       // Obtiene todos los actores
+router.get('/', ActorController.getAllActors);       // Obtiene todos los actores
 router.get('/:id', ActorController.getActorById);         // Obtiene un actor por su ID
-router.post('/add', ActorController.createActor);         // Crea un nuevo actor
-router.put('/update/:id', ActorController.updateActor); // Actualiza un actor por su ID
-router.delete('/delete/:id', ActorController.deleteActor); // Elimina un actor por su ID
+router.post('/', ActorController.createActor);         // Crea un nuevo actor
+router.put('/:id', ActorController.updateActor); // Actualiza un actor por su ID
+router.delete('/:id', ActorController.deleteActor); // Elimina un actor por su ID
 
 // Rutas para manejar imágenes de actores
 router.post('/:idactor/images', upload.single('imagen'), ActorController.addImage); // Subir imagen
