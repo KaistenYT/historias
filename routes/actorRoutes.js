a6ba79fdfec4932905cca138966bd283fa9fd021
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-05-15 20:20:27 -0400

    get

--- routes/actorRoutes.js
+++ routes/actorRoutes.js
@@ -3,12 +3,11 @@ import { ActorController } from '../controller/actorController.js';
 
 const router = express.Router();
 
-// Define las rutas para los recursos de actor
-router.get('/', ActorController.getAllActors);       // Obtiene todos los actores
-router.get('/:id', ActorController.getActorById);         // Obtiene un actor por su ID
-router.post('/', ActorController.createActor);         // Crea un nuevo actor
+// Rutas para actores
+router.get('/', ActorController.getAllActors); // Lista todos los actores
+router.get('/:id', ActorController.getActorById); // Obtiene un actor por ID
+router.post('/', ActorController.createActor); // Crea un nuevo actor
 router.put('/:id', ActorController.updateActor); // Actualiza un actor por su ID
 router.delete('/:id', ActorController.deleteActor); // Elimina un actor por su ID
-
 
 export default router;
