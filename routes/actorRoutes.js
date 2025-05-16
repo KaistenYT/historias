0667e66d9dec2e2f0d548c14460b807f8645cd76
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-05-15 20:17:37 -0400

    fiuuuuu

--- routes/actorRoutes.js
+++ routes/actorRoutes.js
@@ -10,8 +10,5 @@ router.post('/', ActorController.createActor);         // Crea un nuevo actor
 router.put('/:id', ActorController.updateActor); // Actualiza un actor por su ID
 router.delete('/:id', ActorController.deleteActor); // Elimina un actor por su ID
 
-// Rutas para manejar imágenes de actores
-router.post('/:idactor/images', upload.single('imagen'), ActorController.addImage); // Subir imagen
-router.get('/:idactor/image', ActorController.getActorImage); // Obtener imagen
 
 export default router;
