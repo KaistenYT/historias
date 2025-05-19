db58e5e2f59648af5571705e7aae2e964d7143c6
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-05-18 20:04:56 -0400

    cambiamos cositas

--- routes/historyRoutes.js
+++ routes/historyRoutes.js
@@ -7,12 +7,6 @@ const router = express.Router();
 // Configuración de multer
 const storage = multer.memoryStorage();
 
-const upload = multer({
-    storage: storage,
-    limits: {
-      fileSize: 5 * 1024 * 1024, // 5 MB (en bytes)
-    },
-  });
 
 router.get('/list', HistoryController.getAllHistory)
 router.get('/:id', HistoryController.getHistoryById)
@@ -22,7 +16,6 @@ router.delete('/delete/:id', HistoryController.deleteHistory)
 
 
 //mis changes
-
 router.post('/upload-image/:historyId/image', upload.single('file'), HistoryController.uploadHistoryImage); // Sube la imagen de la historia (APLICANDO el middleware)
 router.delete('/delete-image/:historyId/image', HistoryController.deleteHistoryImage); // Elimina la imagen de la historia
 
